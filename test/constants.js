c6d60137d8c71cf184c7cda5229deccb9a023e17
arthas168 <pavlovskipetko@abv.bg>
2020-01-06 11:38:50 +0200

    Deploy contract test added.

--- test/constants.js
+++ test/constants.js
@@ -4,7 +4,7 @@ const constants = {
 	// Common
 	TESTNET: "testnet",
 	SECRET_KEY: "fb65e4673da53618d7ef4d28d8a2b06f1bf52f979ac1a1e9cc47e90713c751356635f86662a34aa89f95eeddada748b3cb930634e8122f0b01997aea9d63d893",
-	COMPILER: "https://compiler.aepps.com",
+	COMPILER_URL: "https://compiler.aepps.com",
   SECONDS_IN_ONE_MINUTE: 60,
   ZERO: 0,
   // Year 2038
--- test/index.js
+++ test/index.js
@@ -1,56 +1,62 @@
 const Deployer = require("aeproject-lib").Deployer;
 const EXAMPLE_CONTRACT_PATH = "./contracts/ExampleContract.aes";
+const { TESTNET, SECRET_KEY, COMPILER_URL } = require("./constants.js");
 
+// Unit tests wrapper
 describe("Example Contract", () => {
   let deployer;
   let instance;
-  let hamsterName;
 
   before(async () => {
-    deployer = new Deployer(
-      "testnet",
-      "fb65e4673da53618d7ef4d28d8a2b06f1bf52f979ac1a1e9cc47e90713c751356635f86662a34aa89f95eeddada748b3cb930634e8122f0b01997aea9d63d893",
-      "https://compiler.aepps.com"
-    );
+    deployer = new Deployer(TESTNET, SECRET_KEY, COMPILER_URL);
   });
 
-  it("Deploying Example Contract", async () => {
-    const deployedPromise = deployer.deploy(EXAMPLE_CONTRACT_PATH); // Deploy it
-
-    await assert.isFulfilled(
-      deployedPromise,
-      "Could not deploy the ExampleContract Smart Contract"
-    ); // Check whether it's deployed
+  beforeEach(async () => {
+    const deployedPromise = deployer.deploy(EXAMPLE_CONTRACT_PATH);
     instance = await Promise.resolve(deployedPromise);
   });
 
-  it("Should check if hamster has been created", async () => {
-    hamsterName = "C.Hamster";
-    await instance.createHamster(hamsterName);
-
-    let exists = (await instance.nameExists(hamsterName)).decodedResult;
-
-    assert.isTrue(exists, "hamster has not been created");
+  // Deploy contract
+  it("should deploy contract", async () => {
+    assert(
+      instance.address !== "",
+      `Expected valid hash for address, got ${instance.address} instead`
+    );
   });
 
-  it("Should REVERT if hamster already exists", async () => {
-    await assert.isRejected(instance.createHamster("C.Hamster"));
-  });
+  // Contract exists
+  // it("should return error, because contract doesn't exist yet", async () => {
+  //   const contractExists = await instance.contractExists(id);
+  //   assert(!contractExists, `Expected false, got ${contractExists} instead`);
+  // });
 
-  it("Should return false if name does not exist", async () => {
-    hamsterName = "DoesHamsterExists";
-    let exists = (await instance.nameExists(hamsterName)).decodedResult;
+  // it("Should check if hamster has been created", async () => {
+  //   hamsterName = "C.Hamster";
+  //   await instance.createHamster(hamsterName);
 
-    assert.isOk(!exists);
-  });
+  //   let exists = (await instance.nameExists(hamsterName)).decodedResult;
 
-  it("Should return true if the name exists", async () => {
-    hamsterName = "DoesHamsterExists";
+  //   assert.isTrue(exists, "hamster has not been created");
+  // });
 
-    await instance.createHamster(hamsterName);
+  // it("Should REVERT if hamster already exists", async () => {
+  //   await assert.isRejected(instance.createHamster("C.Hamster"));
+  // });
 
-    let exists = (await instance.nameExists(hamsterName)).decodedResult;
+  // it("Should return false if name does not exist", async () => {
+  //   hamsterName = "DoesHamsterExists";
+  //   let exists = (await instance.nameExists(hamsterName)).decodedResult;
 
-    assert.isOk(exists);
-  });
+  //   assert.isOk(!exists);
+  // });
+
+  // it("Should return true if the name exists", async () => {
+  //   hamsterName = "DoesHamsterExists";
+
+  //   await instance.createHamster(hamsterName);
+
+  //   let exists = (await instance.nameExists(hamsterName)).decodedResult;
+
+  //   assert.isOk(exists);
+  // });
 });
